f28398e088132ed5e83bd0921afbf95576272508
Mike Klimek <mike@criduchat.de>
2018-12-02 17:33:14 +0100

    Add day 2 part 1

---
+++ Day2/part1.kts
@@ -0,0 +1,17 @@
+import java.io.File
+
+fun String.hasDuplicateLetters(count: Int) = groupBy { it }
+        .values
+        .any { it.size == count}
+
+val lines = File(args[0]).readLines()
+
+val doubles = lines
+    .filter { it.hasDuplicateLetters(2) }
+    .count()
+
+val tripples = lines
+    .filter { it.hasDuplicateLetters(3) }
+    .count()
+
+println("Solution: ${doubles * tripples}")
