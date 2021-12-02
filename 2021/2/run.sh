0e1920ba239f5029873b10199b37b61444aa53bc
Mike Klimek <mike.klimek.developer@gmail.com>
2021-12-02 21:52:42 +0100

    Clean up day 2 code

--- 2021/2/run.sh
+++ 2021/2/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Usage: ./run.sh part1.mlm sample.txt
 
-# transform input, so it is a valid list of strings in arden syntax, e.g. ("foo", "bar")
 cat $2 |
-sed 's/.*/"&"/' | # wrap lines with quotes
-tr '\n' ',' | # replace newlines with commas
-sed 's/.*/(&)/' | # add leading and trailing bracket
+# Transform input, so it is a valid list of strings in arden syntax, e.g. ("foo", "bar")
+sed 's/.*/"&"/' | # Wrap lines with quotes
+tr '\n' ',' | # Replace newlines with commas
+sed 's/.*/(&)/' | # Add leading and trailing bracket
 java -jar arden2bytecode.jar --run $1
